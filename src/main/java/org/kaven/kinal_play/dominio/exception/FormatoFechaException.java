c5e429f216073fc37e7f4c85919701e958a47b32
Angel Lucero <alucero-2021211@kinal.edu.gt>
2025-09-12 14:07:56 -0600

    cambios en clase 12/09/2025

---
+++ src/main/java/org/kaven/kinal_play/dominio/exception/FormatoFechaException.java
@@ -0,0 +1,7 @@
+package org.kaven.kinal_play.dominio.exception;
+
+public class FormatoFechaException extends RuntimeException {
+  public FormatoFechaException(String message) {
+    super(message);
+  }
+}
